6749ba50ed6197f2058289beafaa4033ff1d7ec9
Dalton Riehl <dtonriehl@gmail.com>
2024-02-28 21:28:20 -0600

    restore to default to hopefully fix CORS error in Chrome DevTools

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// index.js
+
+// Callbacks
+const handleClick = (ramen) => {
+  // Add code to display ramen details
+};
+
+const addSubmitListener = () => {
+  // Add code to handle form submission and create new ramen
+}
+
+const displayRamens = () => {
+  // Add code to fetch data from the server and display ramen images
+};
+
+const main = () => {
+
+}
+
+main()
+
+// Export functions for testing
+export {
+  displayRamens,
+  addSubmitListener,
+  handleClick,
+  main,
+};
