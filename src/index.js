4b48fe80019243dc583af8f7929800f385318270
Dalton Riehl <dtonriehl@gmail.com>
2024-02-29 16:18:46 -0600

    Code Challenge Complete: Achievement Get!

--- src/index.js
+++ src/index.js
@@ -1,25 +1,67 @@
-// index.js
+const url = 'http://localhost:3000';
 
-// Callbacks
 const handleClick = (ramen) => {
-  // Add code to display ramen details
-};
+  const detailImage = document.querySelector('.detail-image');
+  const detailName = document.querySelector('.name');
+  const detailRestaurant = document.querySelector('.restaurant');
+  const ratingDisplay = document.getElementById('rating-display');
+  const commentDisplay = document.getElementById('comment-display');
+
+  detailImage.src = ramen.image;
+  detailName.textContent = ramen.name;
+  detailRestaurant.textContent = ramen.restaurant;
+  ratingDisplay.textContent = ramen.rating;
+  commentDisplay.textContent = ramen.comment;
+}
 
 const addSubmitListener = () => {
-  // Add code to handle form submission and create new ramen
+  document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSubmit();
+  });
 }
 
 const displayRamens = () => {
-  // Add code to fetch data from the server and display ramen images
-};
-
-const main = () => {
-
+  const ramenMenu = document.getElementById('ramen-menu');
+  fetch(`${url}/ramens`)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(ramen => {
+        const img = document.createElement('img');
+        img.src = ramen.image;
+        ramenMenu.appendChild(img);
+        img.addEventListener('click', () => handleClick(ramen));
+      });
+    })
+    .catch(error => console.error('Ramen fetch Error:', error));
 }
 
-main()
+const handleSubmit = () => {
+  const ramenMenu = document.getElementById('ramen-menu');
+  const submitButton = document.getElementById('submit-button');
+  const newRamenForm = document.getElementById('new-ramen');
+  const name = document.getElementById('new-name').value;
+  const restaurant = document.getElementById('new-restaurant').value;
+  const image = document.getElementById('new-image').value;
+  const rating = document.getElementById('new-rating').value;
+  const comment = document.getElementById('new-comment').value;
+  const newImage = document.createElement('img');
+  newImage.src = image;
+    newImage.addEventListener('click', () => {
+      handleClick({
+        name, restaurant, image, rating, comment
+      });
+    });
+  ramenMenu.appendChild(newImage);
+  newRamenForm.reset();
+}
 
-// Export functions for testing
+const main = () => {
+  addSubmitListener();
+  displayRamens();
+}
+main();
+
 export {
   displayRamens,
   addSubmitListener,
